dfa9a2249e1c16667d5461e2568a735e403bbf9b
ricardo <especimenraro.cl@gmail.com>
2017-09-27 23:32:45 -0300

    funcionalidad matematica finalizada

--- js/app.js
+++ js/app.js
@@ -3,7 +3,9 @@ var Calculadora = {
 		
 		this.resultado = 0
 		this.operacion=""
+		this.flagResultado = 1
 		sessionStorage.setItem("operacion",this.operacion)
+		sessionStorage.setItem("operacionAnterior","")
 		this.escuchaClick()
 		} ,	// FIN INIT
 	escuchaClick: function () {
@@ -34,9 +36,9 @@ var Calculadora = {
 											break;
 				case "9":	self.asignaOperando(tecla)
 											break;		
-				case "on":self.asignaOperador(tecla)
+				case "on":self.onClear()
 											break;		
-				case "sign":self.asignaOperador(tecla)
+				case "sign":self.cambiaSigno()
 											break;		
 				case "raiz": self.asignaOperador(tecla)
 											break;		
@@ -61,39 +63,74 @@ var Calculadora = {
 		} //FIN FOR
 		
 	}, //FIN ESCUCHA CLICK
-	asignaOperando: function (tecla) {			
+	asignaOperando: function (tecla) {	
+			
+			if (!this.flagResultado) {
+			
 			this.operacion = sessionStorage.getItem("operacion")
 			this.operacion = this.operacion+tecla
 			sessionStorage.setItem("operacion",this.operacion)
 			this.muestraResultado()
+		}//FIN IF
+		else {
+			this.operacion = tecla
+			this.flagResultado = 0
+			sessionStorage.setItem("operacion",this.operacion)
+			this.muestraResultado()		
+		} //FIN ELSE
 	}, //FIN ASIGNA OPERANDO
 	asignaOperador:  function (tecla) {
 			this.operacion = sessionStorage.getItem("operacion")
 			this.operacion =  this.operacion + "," + tecla + ","
 			sessionStorage.setItem("operacion",this.operacion)
+			this.flagResultado=0
 			this.muestraResultado()
 	}, //FIN ASIGNA OPERADOR
 	ejecutaOperacion: function () {
 			arregloElementosOperacion = (sessionStorage.getItem("operacion")).split(",")
+			arregloElementosOperacionAnterior = (sessionStorage.getItem("operacionAnterior")).split(",")
 			var elemento
 			var resultadoParcial
-			var resultado = Number(arregloElementosOperacion[0])
+			var operando1,operando2;
+			if (arregloElementosOperacionAnterior.length == 3 && arregloElementosOperacion.length == 1) {
+				operando1 = arregloElementosOperacion[0]
+				operando2 = Number(arregloElementosOperacionAnterior[2])
+				elemento = arregloElementosOperacionAnterior[1]
+				switch(elemento) {
+				case "+": 	this.resultado = this.resultado +operando2;
+												break;
+				case "-":  	this.resultado = this.resultado - operando2;
+												break;
+				case "x":  	this.resultado = this.resultado * operando2;
+												break;
+				case "/":  	this.resultado = this.resultado / operando2;
+												break;
+				default:
+				} // FIN SWITCH
+			} // FIN IF
+			else {
+			this.resultado = Number(arregloElementosOperacion[0])
 			for (i=1;i<arregloElementosOperacion.length;i=i+2) {
 				elemento = arregloElementosOperacion[i]
 				switch(elemento) {
-				case "+": 	resultado = resultado + Number(arregloElementosOperacion[i+1]);
+				case "+": 	this.resultado = this.resultado + Number(arregloElementosOperacion[i+1]);
 												break;
-				case "-":  	resultado = resultado - Number(arregloElementosOperacion[i+1]);
+				case "-":  	this.resultado = this.resultado - Number(arregloElementosOperacion[i+1]);
 												break;
-				case "x":  	resultado = resultado * Number(arregloElementosOperacion[i+1]);
+				case "x":  	this.resultado = this.resultado * Number(arregloElementosOperacion[i+1]);
 												break;
-				case "/":  	resultado = resultado / Number(arregloElementosOperacion[i+1]);
+				case "/":  	this.resultado = this.resultado / Number(arregloElementosOperacion[i+1]);
 												break;
 				default:
 				} // FIN SWITCH
 			
 			} //FIN FOR
-			sessionStorage.setItem("operacion",resultado.toString())
+			sessionStorage.setItem("operacionAnterior",sessionStorage.getItem("operacion"))			
+		} //FIN ELSE
+			
+			sessionStorage.setItem("operacion",this.resultado.toString())
+			
+			this.flagResultado = 1
 			this.muestraResultado()
 	},//FIN EJECUTA OPERACION
 	
@@ -105,5 +142,25 @@ var Calculadora = {
 			} //FIN FOR
 			pantalla = document.getElementById("display")
 			pantalla.innerHTML = textoOperacion
-	} //FIN MUESTRA RESULTADO
+	},//FIN MUESTRA RESULTADO
+	onClear: function () {
+		this.resultado = 0
+		sessionStorage.setItem("operacion",this.resultado.toString())
+		this.flagResultado = 1
+		this.muestraResultado()
+	},//FIN ONCLEAR
+	cambiaSigno: function () {
+	
+		this.operacion = sessionStorage.getItem("operacion")
+		if (!Number(this.operacion)==false)
+		{
+			this.resultado = -Number(this.operacion)
+			sessionStorage.setItem("operacion",this.resultado.toString())
+		this.flagResultado = 1
+		this.muestraResultado()
+		} //FIN IF
+		
+		
+	} //FIN CAMBIA SIGNO
 } //FIN CALCULADORA
+Calculadora.init()
